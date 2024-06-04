4c96e0c2f79cd83b7f74e3ec5da0e40ba1014d12
Matheus L <matheus.ladeiras@gmail.com>
2024-06-04 14:33:02 -0300

    WrightWilsonNoSolution

--- index.js
+++ index.js
@@ -87,24 +87,20 @@ function WilsonMazeGenerator() {
     for(let i = 1; i < 1025; i++) {
         notVisited.push(i);
     }
+    let randCell;
     while (visited.length < 1024) {
-    //for(let u = 0; u < 100; u++) {
         console.log("Novo caminho");
+        if(visited.length == 0) {
+            randCell = notVisited[Math.floor(Math.random()*notVisited.length)];
+            console.log("randCell:"+randCell);
+            visited.push(randCell);
+            notVisited.splice(notVisited.indexOf(randCell), 1);
+        }
         let start = notVisited[Math.floor(Math.random()*notVisited.length)];
         notVisited.splice(notVisited.indexOf(start), 1);
-        visited.push(start);
-        let randCell;
-        if (notVisited.length != 0) {
-            randCell = notVisited[Math.floor(Math.random()*notVisited.length)];
-        } else {
-            randCell = visited[Math.floor(Math.random()*notVisited.length)]; //ver a possibilidade de se achar,
-        }
-        let path = [randCell];
-        console.log(start);
-        console.log(randCell);
+        let path = [start];
         let directions = [];
         do {
-            //console.log("Novo step");
             let [top, bot, right, left] = Array(4).fill(0);
             possibilities = [];
             if (path[path.length-1] > 32) {
@@ -124,6 +120,7 @@ function WilsonMazeGenerator() {
                 possibilities.push(left);
             };
             console.log("possibilities:"+possibilities);
+            console.log("Visited:"+visited);
             nextStep = possibilities[Math.floor(Math.random()*possibilities.length)];
             path.push(nextStep);
             switch (nextStep) {
@@ -136,17 +133,12 @@ function WilsonMazeGenerator() {
                 case left:
                     directions.push(4); break;
                 }
-                if(visited.includes(nextStep) == true) {
-                    directions.push(0);
-                    console.log("Full path: "+path);
-                    console.log("Full directions: "+directions);
-                }
-        } while (visited.includes(nextStep) == false) //
+        } while (visited.includes(nextStep) == false) 
         //Retirar start dos visitados caso ele tenha sido ignorado com o caminho chegando em outra ala do labirinto
-        if(path.includes(start) == false && notVisited.length != 0) {
+        /*if(path.includes(start) == false && notVisited.length != 0) {
             visited.pop(start);
             notVisited.push(start);
-        }
+        }*/
         //limpar o path
         for(let i = 0; i < path.length; i++) {
             let step = path[i];  
@@ -174,29 +166,21 @@ function WilsonMazeGenerator() {
                     console.log("Top");
                     document.getElementById(step.toString()).style.borderTop="0px";
                     document.getElementById(nextStep.toString()).style.borderBottom="0px";
-                    //document.getElementById(step.toString()).style.backgroundColor="lightblue";
-                    //document.getElementById(nextStep.toString()).style.backgroundColor="red";
                     break;
                 case 2:
                     console.log("Bot");
                     document.getElementById(step.toString()).style.borderBottom="0px";
                     document.getElementById(nextStep.toString()).style.borderTop="0px";
-                    //document.getElementById(step.toString()).style.backgroundColor="lightblue";
-                    //document.getElementById(nextStep.toString()).style.backgroundColor="red";
                     break;
                 case 3:
                     console.log("Right");
                     document.getElementById(step.toString()).style.borderRight="0px";
                     document.getElementById(nextStep.toString()).style.borderLeft="0px";
-                    //document.getElementById(step.toString()).style.backgroundColor="lightblue";
-                    //document.getElementById(nextStep.toString()).style.backgroundColor="red";
                     break;
                 case 4:
                     console.log("Left");
                     document.getElementById(step.toString()).style.borderLeft="0px";
                     document.getElementById(nextStep.toString()).style.borderRight="0px";
-                    //document.getElementById(step.toString()).style.backgroundColor="lightblue";
-                    //document.getElementById(nextStep.toString()).style.backgroundColor="red";
                     break;
             }
         }  
@@ -224,140 +208,3 @@ function WilsonMazeGenerator() {
 //Quando só sobra um elemento não visitado e esse é usado no start ele não sabe oq fazer com o último elemento
 
 //Todos os starts não mostram efeito algum, eles não são implementados no path provavelmente
-
-
-
-
-function WilsonMazeGeneratorWikipedia() {
-    RestoreMaze();
-    let visited = [];
-    let notVisited = [];
-    let directions = [];
-    for(let i = 1; i < 1025; i++) {
-        notVisited.push(i);
-    }
-    let o = 0;
-    let stop = 0;
-    do {
-        let start = notVisited[Math.floor(Math.random()*notVisited.length)];
-        notVisited.splice(notVisited.indexOf(start), 1);
-        if(visited.includes(start) == false) {visited.push(start)};
-        //visited.push(start);
-        let end = notVisited[Math.floor(Math.random()*notVisited.length)];
-        console.log(start);
-        console.log(end);
-        let path = [start];
-        //Presisa de um meio de parar o loop quando achar algum caminho já feito e só ligar os 2
-        while (stop < 100000 && path.includes(end) == false ) {//&& visited.includes(path[path.length-1]) == false
-            let possibilities = [];
-            let nextStep;
-            let [top, bot, right, left] = Array(4).fill(0);
-            if (path[path.length-1] > 32) {
-                top = path[path.length-1] - 32;
-                possibilities.push(top);
-            };
-            if (path[path.length-1] < 993) {
-                bot = path[path.length-1] + 32;
-                possibilities.push(bot);
-            };
-            if (path[path.length-1] % 32 !== 0) {
-                right = path[path.length-1] + 1;
-                possibilities.push(right);
-            };
-            if ((path[path.length-1] - 1) % 32 !== 0) {
-                left = path[path.length-1] - 1;
-                possibilities.push(left);
-            };
-            console.log("possibilities:"+possibilities);
-            nextStep = possibilities[Math.floor(Math.random()*possibilities.length)];
-            if(path.includes(nextStep) == true) {
-                while (path[path.length-1] != nextStep) {
-                    path.pop();
-                    directions.pop();
-                } 
-            } else {
-                path.push(nextStep);
-                //Apenas quando o caminho completo for criado que se lê um por um e implementa o caminho
-                //Guardar direções em objetos ou só array
-                switch (nextStep) {
-                    case top:
-                        //console.log("Top");
-                        directions.push(1);
-                        break;
-                    case bot:
-                        //console.log("Bot");
-                        directions.push(2);
-                        break;
-                    case right:
-                        //console.log("Right");
-                        directions.push(3);
-                        break;
-                    case left:
-                        //console.log("Left");
-                        directions.push(4);
-                        break;
-                    }
-            }  
-            //console.log(nextStep);
-            if (visited.includes(nextStep) == false) {
-                o++
-                if(o > 1) {
-                    break;
-                }
-            }
-            stop++;
-        } //while((stop < 10) || (path.includes(end) === false)); //path.includes(end) == false || visited.includes(nextStep) == false || stop <10
-        console.log("Foi");
-        console.log("path.length:"+path.length);
-        for(let i = 0; i < path.length; i++) {console.log((i+1)+")path:"+path[i])}
-        
-        //Começa do primeiro elemento que é start, mas esse já está lá, do segundo em diante que retiramos
-        console.log("directions.length:"+directions.length)
-        do {
-        //for(let i = 0; i < path.length; i++) {
-            let step = path.shift(); //path[0];
-            console.log("directions:"+directions)
-            if(visited.includes(step) == false) {visited.push(step)};
-            if(notVisited.includes(step) == true) {notVisited.splice(notVisited.indexOf(step), 1);};
-            let direction = directions.shift();
-            //console.log("path[path.length-1]:"+path[path.length-1])
-            switch (direction) {
-                case 1: //Tem que pegar o caminho intercalando os valores com as direções e pegando o valor do start
-                    console.log("Top");
-                    document.getElementById(step.toString()).style.borderTop="0px";
-                    document.getElementById(path[0].toString()).style.borderBottom="0px";
-                    break;
-                case 2:
-                    console.log("Bot");
-                    document.getElementById(step.toString()).style.borderBottom="0px";
-                    document.getElementById(path[0].toString()).style.borderTop="0px";
-                    break;
-                case 3:
-                    console.log("Right");
-                    document.getElementById(step.toString()).style.borderRight="0px";
-                    document.getElementById(path[0].toString()).style.borderLeft="0px";
-                    break;
-                case 4:
-                    console.log("Left");
-                    document.getElementById(step.toString()).style.borderLeft="0px";
-                    document.getElementById(path[0].toString()).style.borderRight="0px";
-                    break;
-                }
-            //path.shift();
-        } while (path.length != 0)
-        console.log("Completou fazer o caminho de um elemento.");
-        console.log(visited);
-        console.log(stop);
-    } while (visited.length < 1024);
-}
-
-/*
-    stack.push(2);
-    stack.push(3);
-    stack.pop();
-    stack.pop();
-    console.log(stack);
-
-    Fazer o caminho pode usar isso aqui:
-    document.getElementById("1").style.background="blue";
-    */
