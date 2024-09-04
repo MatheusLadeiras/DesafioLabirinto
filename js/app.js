dc53ec9a5e4d04f3b5883a972e422eeda882bd70
MatheusLadeiras <69425532+MatheusLadeiras@users.noreply.github.com>
2024-09-04 18:51:37 -0300

    expressWorking

--- js/app.js
+++ js/app.js
@@ -3,13 +3,14 @@ const express = require('express')
   const app = express()
   const port = 3000
 
-  var path = require('path');
+  let path = require('path');
   //es.sendFile(path.resolve(__dirname+'/../index.html'));
+  let parentDir = path.normalize(__dirname+"/.."); // Outra alternativa uma pasta atrás: /../..   '/DesafioLabirinto/html/index.html'
 
+  
   app.get('/', (req, res) => {
-    var parentDir = path.normalize(__dirname+"/.."); // Outra alternativa uma pasta atrás: /../..   '/DesafioLabirinto/html/index.html'
     res.sendFile(parentDir+'/html/index.html');
-    app.use(express.static("."));
+    //app.use(express.static("."));
     /* This also dont work
     res.sendFile(parentDir+'/css/index.css');
     res.sendFile(parentDir+'/js/index.js');
@@ -25,8 +26,34 @@ const express = require('express')
     //path.join(__dirname, '..', 'html', 'index.html') //Testar algo com isso mas tem que ver como isso seria referenciado pelo res
   })
 
-  app.use(express.static('.'))
+  app.get('/js', (req, res) => {
+    res.sendFile(parentDir+'/js/index.js');
+  })
+
+  app.get('/css', (req, res) => {
+    res.sendFile(parentDir+'/css/index.css');
+  });
+
+  app.get('/media/funky_15secCut.mp3', (req, res) => {
+    res.sendFile(parentDir+'/media/funky_15secCut.mp3');
+  });
+
+  app.get('/media/faviconMaze.jpg', (req, res) => {
+    res.sendFile(parentDir+'/media/faviconMaze.jpg');
+  });
+
+  app.get('/media/start_cropped_image.png', (req, res) => {
+    res.sendFile(parentDir+'/media/start_cropped_image.png');
+  });
+
+  app.get('/media/goal_cropped_image.png', (req, res) => {
+    res.sendFile(parentDir+'/media/goal_cropped_image.png');
+  });
+
+  app.get('/media/cropped_Arrow.png', (req, res) => {
+    res.sendFile(parentDir+'/media/cropped_Arrow.png');
+  });
 
   app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
-})
+  })
